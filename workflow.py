86911b9bb5212f9e25726f848ffe5259c0684e80
Turgut Yigit Akyol tya@mbg.au.dk <tyakyol@fe1.genomedk.net>
2020-05-18 14:27:48 +0200

    directories were changed.

--- workflow.py
+++ workflow.py
@@ -2,49 +2,49 @@ from gwf import Workflow
 from templates import *
 import glob
 
-files1 = sorted(glob.glob('data/*_R1.fastq'))
-files2 = sorted(glob.glob('data/*_R2.fastq'))
+files1 = sorted(glob.glob('../../InRoot/Backup/data/01_20200402_137_LjAcessions_fastq/*_R1.fastq'))
+files2 = sorted(glob.glob('../../InRoot/Backup/data/01_20200402_137_LjAcessions_fastq/*_R2.fastq'))
 files = list(zip(files1, files2))
-rg = 'data/refGen.fa'
+rg = '../../InRoot/Backup/data/02_20200402_Gifu1.2_ref_data/LjGifu1.1_pseudomol.fa'
 
 gwf = Workflow()
 
 gwf.target_from_template('bwaIndex',
                          bwa_index(fa=rg,
-                                   amb=rg + '.amb',
-                                   ann=rg + '.ann',
-                                   bwt=rg + '.bwt',
-                                   pac=rg + '.pac',
-                                   sa=rg + '.sa'))
+                                   amb=rg+'.amb',
+                                   ann=rg+'.ann',
+                                   bwt=rg+'.bwt',
+                                   pac=rg+'.pac',
+                                   sa=rg+'.sa'))
 
 bam_files = []
 
 for i in range(len(files)):
-        gwf.target_from_template('bwaMapping_{}'.format(files[i][0][11:16]),
+        gwf.target_from_template('bwaMapping_{}'.format(files[i][0][59:65]),
                                  bwa_map(fa=rg,
                                          fq1=files[i][0],
                                          fq2=files[i][1],
                                          output='results/mapped_{}.bam'.format(
-                                             files[i][0][11:16])
+                                             files[i][0][59:65])
                                          ))
 
-        gwf.target_from_template('samtoolsSort_{}'.format(files[i][0][11:16]),
+        gwf.target_from_template('samtoolsSort_{}'.format(files[i][0][59:65]),
                                  samtools_sort(
                                      mapped='results/mapped_{}.bam'.format(
-                                         files[i][0][11:16]),
+                                         files[i][0][59:65]),
                                      sorted_='results/sorted_{}.bam'.format(
-                                         files[i][0][11:16])
+                                         files[i][0][59:65])
                                      ))
 
-        gwf.target_from_template('samtoolsIndex_{}'.format(files[i][0][11:16]),
+        gwf.target_from_template('samtoolsIndex_{}'.format(files[i][0][59:65]),
                                  samtools_index(
                                      bam='results/sorted_{}.bam'.format(
-                                         files[i][0][11:16]),
+                                         files[i][0][59:65]),
                                      bai='results/sorted_{}.bam.bai'.format(
-                                         files[i][0][11:16])
+                                         files[i][0][59:65])
                                      ))
 
-        bam_files.append('results/sorted_{}.bam'.format(files[i][0][11:16]))
+        bam_files.append('results/sorted_{}.bam'.format(files[i][0][59:65]))
 
 gwf.target_from_template('bamList',
                          bam_list(bam=bam_files,
@@ -54,5 +54,5 @@ gwf.target_from_template('bamList',
 gwf.target_from_template('bcftoolsCall',
                          bcftools_call(fa=rg,
                                        bamlist='results/bam_list.txt',
-                                       output='results/calls.vcf'
+                                       output='results/raw_variants.vcf'
                                         ))
